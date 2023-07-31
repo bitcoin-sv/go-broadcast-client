cc16c2608a9a9241970071dec271bca589bd454c
wregulski <wojciechregulski@outlook.com>
2023-07-31 18:06:54 +0200

    fix: adds given/when/then and proper names

--- internal/acceptance_tests/arc_query_tx_test.go
+++ internal/acceptance_tests/arc_query_tx_test.go
@@ -32,70 +32,72 @@ var secondSuccessfulResponse = `
 `
 
 func TestQueryTransaction(t *testing.T) {
-	t.Run("Happy Path - Query success from arc1 and arc2", func(t *testing.T) {
+	t.Run("Should successfully query from multiple ArcClients", func(t *testing.T) {
+		// given
 		httpClientMock := &arc.MockHttpClient{}
-
 		arc1 := arc.NewArcClient(config.ArcClientConfig{APIUrl: "http://arc1-api-url", Token: "arc1-token"}, httpClientMock)
 		arc2 := arc.NewArcClient(config.ArcClientConfig{APIUrl: "http://arc2-api-url", Token: "arc2-token"}, httpClientMock)
-
 		broadcaster := broadcast.NewCompositeBroadcaster(config.DefaultStrategy, arc.CreateMockArcFactory(arc1), arc.CreateMockArcFactory(arc2))
-
 		httpResponse1 := &http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(strings.NewReader(firstSuccessfulResponse))}
 		httpResponse2 := &http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(strings.NewReader(secondSuccessfulResponse))}
-
 		httpClientMock.On("DoRequest", mock.Anything, mock.Anything).Return(httpResponse1, nil).Once()
 		httpClientMock.On("DoRequest", mock.Anything, mock.Anything).Return(httpResponse2, nil).Once()
 
+		// when
 		result, err := broadcaster.QueryTransaction(context.Background(), "txID")
+
+		// then
 		assert.NoError(t, err)
 		assert.NotNil(t, result)
 	})
 
-	t.Run("Sad Path - All ArcClients return errors", func(t *testing.T) {
+	t.Run("Should return error if all ArcClients return errors", func(t *testing.T) {
+		// given
 		httpClientMock := &arc.MockHttpClient{}
-
 		arc1 := arc.NewArcClient(config.ArcClientConfig{APIUrl: "http://arc1-api-url", Token: "arc1-token"}, httpClientMock)
 		arc2 := arc.NewArcClient(config.ArcClientConfig{APIUrl: "http://arc2-api-url", Token: "arc2-token"}, httpClientMock)
-
 		broadcaster := broadcast.NewCompositeBroadcaster(config.DefaultStrategy, arc.CreateMockArcFactory(arc1), arc.CreateMockArcFactory(arc2))
-
 		httpResponse := &http.Response{}
 		httpClientMock.On("DoRequest", mock.Anything, mock.Anything).Return(httpResponse, errors.New("http error")).Twice()
 
+		// when
 		result, err := broadcaster.QueryTransaction(context.Background(), "txID")
+
+		// then
 		assert.Error(t, err)
 		assert.Nil(t, result)
 		assert.EqualError(t, err, shared.ErrAllBroadcastersFailed.Error())
 	})
 
-	t.Run("Happy Path - Query success from single arc", func(t *testing.T) {
+	t.Run("Should successfully query from single ArcClient", func(t *testing.T) {
+		// given
 		httpClientMock := &arc.MockHttpClient{}
-
 		arc1 := arc.NewArcClient(config.ArcClientConfig{APIUrl: "http://arc1-api-url", Token: "arc1-token"}, httpClientMock)
-
 		broadcaster := broadcast.NewCompositeBroadcaster(config.DefaultStrategy, arc.CreateMockArcFactory(arc1))
-
 		httpResponse1 := &http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(strings.NewReader(firstSuccessfulResponse))}
 		httpClientMock.On("DoRequest", mock.Anything, mock.Anything).Return(httpResponse1, nil).Once()
 
+		// when
 		result, err := broadcaster.QueryTransaction(context.Background(), "txID")
+
+		// then
 		assert.NoError(t, err)
 		assert.NotNil(t, result)
 	})
 
-	t.Run("Sad Path - Query error from single arc", func(t *testing.T) {
+	t.Run("Should return error if single ArcClient returns error", func(t *testing.T) {
+		// given
 		httpClientMock := &arc.MockHttpClient{}
-
 		arc1 := arc.NewArcClient(config.ArcClientConfig{APIUrl: "http://arc1-api-url", Token: "arc1-token"}, httpClientMock)
-
 		broadcaster := broadcast.NewCompositeBroadcaster(config.DefaultStrategy, arc.CreateMockArcFactory(arc1))
-
 		httpResponse := &http.Response{}
 		httpClientMock.On("DoRequest", mock.Anything, mock.Anything).Return(httpResponse, errors.New("http error")).Once()
 
+		// when
 		result, err := broadcaster.QueryTransaction(context.Background(), "txID")
+
+		// then
 		assert.Error(t, err)
 		assert.Nil(t, result)
 	})
-
 }
