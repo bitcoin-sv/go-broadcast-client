5fa1e0be22ee198e2cb693153bd9011a25f027d8
wregulski <wojciechregulski@outlook.com>
2023-08-12 09:10:47 +0200

    feat: handles decoding error instead of ignoring it

--- broadcast/internal/arc/utils/arc_utils.go
+++ broadcast/internal/arc/utils/arc_utils.go
@@ -3,11 +3,14 @@ package arcutils
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 
 	"github.com/bitcoin-sv/go-broadcast-client/broadcast"
 	"github.com/bitcoin-sv/go-broadcast-client/broadcast/internal/httpclient"
 )
+
+var ErrUnableToDecodeArcError = errors.New("unable to decode arc error")
 
 func HandleHttpError(httpClientError error) error {
 	noSuccessResponseErr, ok := httpClientError.(httpclient.HttpClientError)
@@ -51,8 +54,11 @@ func decodeArcError(httpErr httpclient.HttpClientError) error {
 	bodyReader := io.TeeReader(response.Body, &buffer)
 
 	resultError := broadcast.ArcError{}
-	// nolint:errcheck
-	json.NewDecoder(bodyReader).Decode(&resultError) // ignore decoding error
+	err := json.NewDecoder(bodyReader).Decode(&resultError)
+
+	if err != nil {
+		return ErrUnableToDecodeArcError
+	}
 
 	if resultError.Title != "" {
 		return resultError
