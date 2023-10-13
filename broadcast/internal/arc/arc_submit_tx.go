22cd7e02302f009aacbb1b53e8eebb1f777a2cb0
Kuba <127198012+kuba-4chain@users.noreply.github.com>
2023-10-13 14:27:17 +0200

    feat(BUX-236): transaction submitting in RawTx format for backwards compatibility (#48)
    * feat(BUX-236): transaction submitting in RawTx format for backwards compatibility
    
    This reverts commit 7939ff84b43430b119334ae93d2f1692af91c80b.
    
    * feat(BUX-236): supporting RawTx transaction submitting without junglebus
    
    * feat(BUX-236): extracing conversion methods

--- broadcast/internal/arc/arc_submit_tx.go
+++ broadcast/internal/arc/arc_submit_tx.go
@@ -2,9 +2,13 @@ package arc
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"strconv"
+
+	"github.com/libsv/go-bt/v2"
 
 	"github.com/bitcoin-sv/go-broadcast-client/broadcast"
 	arc_utils "github.com/bitcoin-sv/go-broadcast-client/broadcast/internal/arc/utils"
@@ -38,7 +42,7 @@ func (a *ArcClient) SubmitTransaction(ctx context.Context, tx *broadcast.Transac
 
 	response := &broadcast.SubmitTxResponse{
 		BaseResponse: broadcast.BaseResponse{Miner: a.apiURL},
-		SubmittedTx: result,
+		SubmittedTx:  result,
 	}
 
 	return response, nil
@@ -77,7 +81,7 @@ func (a *ArcClient) SubmitBatchTransactions(ctx context.Context, txs []*broadcas
 
 func submitTransaction(ctx context.Context, arc *ArcClient, tx *broadcast.Transaction, opts *broadcast.TransactionOpts) (*broadcast.SubmittedTx, error) {
 	url := arc.apiURL + arcSubmitTxRoute
-	data, err := createSubmitTxBody(tx)
+	data, err := createSubmitTxBody(arc, tx, opts.TransactionFormat)
 	if err != nil {
 		return nil, err
 	}
@@ -109,7 +113,7 @@ func submitTransaction(ctx context.Context, arc *ArcClient, tx *broadcast.Transa
 
 func submitBatchTransactions(ctx context.Context, arc *ArcClient, txs []*broadcast.Transaction, opts *broadcast.TransactionOpts) ([]*broadcast.SubmittedTx, error) {
 	url := arc.apiURL + arcSubmitBatchTxsRoute
-	data, err := createSubmitBatchTxsBody(txs)
+	data, err := createSubmitBatchTxsBody(arc, txs, opts.TransactionFormat)
 	if err != nil {
 		return nil, err
 	}
@@ -139,10 +143,13 @@ func submitBatchTransactions(ctx context.Context, arc *ArcClient, txs []*broadca
 	return model, nil
 }
 
-func createSubmitTxBody(tx *broadcast.Transaction) ([]byte, error) {
-	body := &SubmitTxRequest{tx.RawTx}
-	data, err := json.Marshal(body)
+func createSubmitTxBody(arc *ArcClient, tx *broadcast.Transaction, txFormat broadcast.TransactionFormat) ([]byte, error) {
+	body, err := formatTxRequest(arc, tx, txFormat)
+	if err != nil {
+		return nil, err
+	}
 
+	data, err := json.Marshal(body)
 	if err != nil {
 		return nil, ErrSubmitTxMarshal
 	}
@@ -150,10 +157,14 @@ func createSubmitTxBody(tx *broadcast.Transaction) ([]byte, error) {
 	return data, nil
 }
 
-func createSubmitBatchTxsBody(txs []*broadcast.Transaction) ([]byte, error) {
+func createSubmitBatchTxsBody(arc *ArcClient, txs []*broadcast.Transaction, txFormat broadcast.TransactionFormat) ([]byte, error) {
 	rawTxs := make([]*SubmitTxRequest, 0, len(txs))
 	for _, tx := range txs {
-		rawTxs = append(rawTxs, &SubmitTxRequest{RawTx: tx.RawTx})
+		requestTx, err := formatTxRequest(arc, tx, txFormat)
+		if err != nil {
+			return nil, err
+		}
+		rawTxs = append(rawTxs, requestTx)
 	}
 
 	data, err := json.Marshal(rawTxs)
@@ -202,4 +213,111 @@ func validateSubmitTxResponse(model *broadcast.SubmittedTx) error {
 	}
 
 	return nil
+}
+
+func formatTxRequest(arc *ArcClient, tx *broadcast.Transaction, txFormat broadcast.TransactionFormat) (*SubmitTxRequest, error) {
+	var (
+		body *SubmitTxRequest
+		err  error
+	)
+
+	switch txFormat {
+	case broadcast.RawTxFormat:
+		body, err = rawTxRequest(arc, tx.Hex)
+	case broadcast.EfFormat:
+		body, err = efTxRequest(tx.Hex)
+	case broadcast.BeefFormat:
+		body, err = beefTxRequest(tx.Hex)
+	default:
+		err = fmt.Errorf("unknown transaction format")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func efTxRequest(rawTx string) (*SubmitTxRequest, error) {
+	request := &SubmitTxRequest{RawTx: rawTx}
+
+	return request, nil
+}
+
+func beefTxRequest(rawTx string) (*SubmitTxRequest, error) {
+	return nil, fmt.Errorf("Submitting transactions in BEEF format is unimplemented yet...")
+}
+
+func rawTxRequest(arc *ArcClient, rawTx string) (*SubmitTxRequest, error) {
+	transaction, err := bt.NewTxFromString(rawTx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, input := range transaction.Inputs {
+		if err = updateUtxoWithMissingData(arc, input); err != nil {
+			return nil, err
+		}
+	}
+
+	request := &SubmitTxRequest{
+		RawTx: hex.EncodeToString(transaction.ExtendedBytes()),
+	}
+	return request, nil
+}
+
+func updateUtxoWithMissingData(arc *ArcClient, input *bt.Input) error {
+	txid := input.PreviousTxIDStr()
+
+	pld := httpclient.NewPayload(
+		httpclient.GET,
+		fmt.Sprintf("https://junglebus.gorillapool.io/v1/transaction/get/%s", txid),
+		"",
+		nil,
+	)
+	resp, err := arc.HTTPClient.DoRequest(context.Background(), pld)
+	if err != nil {
+		return err
+	}
+
+	var tx *junglebusTransaction
+	err = arc_utils.DecodeResponseBody(resp.Body, &tx)
+	if err != nil {
+		return err
+	}
+
+	actualTx, err := bt.NewTxFromBytes(tx.Transaction)
+	if err != nil {
+		return err
+	}
+
+	o := actualTx.Outputs[input.PreviousTxOutIndex]
+	input.PreviousTxScript = o.LockingScript
+	input.PreviousTxSatoshis = o.Satoshis
+	return nil
+}
+
+type junglebusTransaction struct {
+	ID          string `json:"id"`
+	Transaction []byte `json:"transaction"`
+	BlockHash   string `json:"block_hash"`
+	BlockHeight uint32 `json:"block_height"`
+	BlockTime   uint32 `json:"block_time"`
+	BlockIndex  uint64 `json:"block_index"`
+
+	// index data
+	// input/output types are
+	// p2pkh, p2sh, token-stas, opreturn, tokenized, metanet, bitcom, run, map, bap, non-standard etc.
+	Addresses   []string `json:"addresses"`
+	Inputs      []string `json:"inputs"`
+	Outputs     []string `json:"outputs"`
+	InputTypes  []string `json:"input_types"`
+	OutputTypes []string `json:"output_types"`
+	Contexts    []string `json:"contexts"`     // optional contexts of output types, only for known protocols
+	SubContexts []string `json:"sub_contexts"` // optional sub-contexts of output types, only for known protocols
+	Data        []string `json:"data"`         // optional data of output types, only for known protocols
+
+	// the merkle proof in binary
+	MerkleProof []byte `json:"merkle_proof"`
 }
